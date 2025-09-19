65b47e81b687327dd3dbfe460aca1cd62a60794a
rubencala30 <rubencala30@gmail.com>
2025-09-19 15:52:58 +0200

    pagina login y register

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
 import "../styles/style.css";
+import { useUserContext } from "../providers/UserProvider";
 
 const Layout = ({ children }) => {
+  const { user } = useUserContext();
   return (
     <div>
       <div className="container_nav">
         <ul className="nav">
           <li>Inicio</li>
           <li>Eventos</li>
-          <Link to="/profile">profile</Link>
+          {user ? (
+            <Link to="/profile">Perfil</Link>
+          ) : (
+            <>
+              <Link to="/login">Inicio de Sesión</Link>
+              <Link to="/register">Registro</Link>
+              <Link to="/profile">Perfil</Link>
+            </>
+          )}
         </ul>
       </div>
       <main>{children}</main>
