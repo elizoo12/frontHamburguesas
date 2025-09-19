575751a76c71a59e92742f9fe25ed604b99aff8b
rubencala30 <rubencala30@gmail.com>
2025-09-19 15:16:00 +0200

    Limpieza App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import Layout from "./components/Layout";
 import Router from "./Router";
 
 function App() {
-
-  return (
-      <Router />
-
-
-  )
+  return <Router />;
 }
 
 export default App;
