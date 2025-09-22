f37b31d649aa09a5d6e2c8eb887472dde1ac9269
Rene <poencima22@gmail.com>
2025-09-22 18:44:35 +0200

    profile page mediun done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,24 +1,40 @@
-import { Link } from "react-router-dom";
-import "../assets/styles/profile.css"
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import "../assets/stylesComponents/profile.css"
+
 
 const Profile = () => {
+    const navigate = useNavigate();
+    const [userData, setUserData] = useState();
+
+    useEffect(() => {
+
+    },[]);
+
+
     return (
         
         <>
-
             <div className="navbar">
-                <div className="nav-home">
+                <div className="nav-home" onClick={() => {navigate("/")}} >
 
                     <img className="link_logo" src="src/assets/images/png/logo.png" alt="logoImage" />
-                    <Link to={"/"} >AuraEvents</Link>
+                    <p to={"/"} >AuraEvents</p>
 
                 </div>
 
             </div>
 
-            <div className="img-phrase" >
+            <div className="content">
 
-                <img src="/src/assets/images/jpg/espejo.jpg" alt="Men watching in the mirrow" />
+                <div className="data-user">
+                    <div className="images">
+                        <img className="img-portada-large" src="/src/assets/images/png/image_large_user.png" alt="imagen de portada" />
+                        <img className="img-circle" src="/src/assets/images/png/img-circle.png" alt="imagen circular de portada" />
+                    </div>
+
+                </div>
 
             </div>
 
